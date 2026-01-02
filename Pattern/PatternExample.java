5f4b69d5b74b18efc66776e062336149467b1611
sarath0508 <itssarath05@gmail.com>
2026-01-02 14:41:11 +0530

    completed inverted right pyramid

--- Pattern/PatternExample.java
+++ Pattern/PatternExample.java
@@ -24,6 +24,12 @@ public class PatternExample {
             for(int j = 1; j <= i; j++)
                 System.out.print(i+" ");
     }
+
+    private static void invertedRightPyramid(int n){
+        for(int i = 0; i < n; i++,System.out.println())
+            for(int j = n-i; j > 0 ;j--)
+                System.out.print("* ");
+    }
     public static void main(String[] args){
         Scanner s = new Scanner(System.in);
         int n = s.nextInt();
@@ -31,7 +37,9 @@ public class PatternExample {
         // rectangularStar(n);
         // rightAngledTriangle(n);
         // rightAngeledNumbers(n);
-        rightAngeledNumbersRepeating(n);
+        // rightAngeledNumbersRepeating(n);
+        invertedRightPyramid(n);
+        
         s.close();
     }
 }
