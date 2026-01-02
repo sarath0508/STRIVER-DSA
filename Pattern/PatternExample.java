2c05829af6a4786e8935cee69ca00711d5b6431a
sarath0508 <itssarath05@gmail.com>
2026-01-02 15:02:00 +0530

    completed star pyramid

--- Pattern/PatternExample.java
+++ Pattern/PatternExample.java
@@ -36,6 +36,16 @@ public class PatternExample {
             for(int j = 1; j <= i; j++)
                 System.out.print(j + " ");
     }
+
+    private static void starPyramid(int n){
+        for(int i = 0; i < n; i++,System.out.println()){
+            for(int j = i; j < n-1; j++)
+                System.out.print(" ");
+            for(int k = 0; k <= 2*i; k++)
+                System.out.print("*");
+
+        }
+    }
     public static void main(String[] args){
         Scanner s = new Scanner(System.in);
         int n = s.nextInt();
@@ -45,8 +55,8 @@ public class PatternExample {
         // rightAngeledNumbers(n);
         // rightAngeledNumbersRepeating(n);
         // invertedRightPyramid(n);
-        invertedRightNumber(n);
-
+        // invertedRightNumber(n);
+        starPyramid(n);
         s.close();
     }
 }
