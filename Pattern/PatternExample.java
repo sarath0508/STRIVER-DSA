6ff7cb886f49d02c32e316beedb7e5d43c92e261
sarath0508 <itssarath05@gmail.com>
2026-01-02 14:21:36 +0530

    completed right angled star pattern

--- Pattern/PatternExample.java
+++ Pattern/PatternExample.java
@@ -6,11 +6,18 @@ public class PatternExample {
             for(int j = 0; j < n; j++)
                 System.out.print("* ");
     }
+    
+    private static void rightAngledTriangle(int n){
+        for(int i = 0; i < n; i++, System.out.println())
+            for(int j = 0; j <= i; j++)
+                System.out.print("* ");
+    }
     public static void main(String[] args){
         Scanner s = new Scanner(System.in);
         int n = s.nextInt();
 
-        rectangularStar(n);
+        // rectangularStar(n);
+        rightAngledTriangle(n);
         s.close();
     }
 }
