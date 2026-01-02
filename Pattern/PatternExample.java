88c3abd40de29c172399f13ab2e8b50022e578e6
sarath0508 <itssarath05@gmail.com>
2026-01-02 14:33:26 +0530

    completed right angled numbers

--- Pattern/PatternExample.java
+++ Pattern/PatternExample.java
@@ -12,12 +12,19 @@ public class PatternExample {
             for(int j = 0; j <= i; j++)
                 System.out.print("* ");
     }
+
+    private static void rightAngeledNumbers(int n){
+        for(int i = 1; i <= n; i++,System.out.println())
+            for(int j = 1; j <= i; j++)
+                System.out.print(j+" ");
+    }
     public static void main(String[] args){
         Scanner s = new Scanner(System.in);
         int n = s.nextInt();
 
         // rectangularStar(n);
-        rightAngledTriangle(n);
+        // rightAngledTriangle(n);
+        rightAngeledNumbers(n);
         s.close();
     }
 }
