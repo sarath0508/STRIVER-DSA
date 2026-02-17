c5eb4be99f330bff31b87d1837b0277bbe089b6d
sarath0508 <itssarath05@gmail.com>
2026-02-17 15:07:57 +0530

    using recuriosn

--- DP/ClimbingStairs.java
+++ DP/ClimbingStairs.java
@@ -1,5 +1,14 @@
+import java.util.Scanner;
 class ClimbingStairs {
+    /*Using Recursion */
+    private static int Solve(int n){
+        if(n <= 2)
+            return n;
+        return Solve(n-1) + Solve(n-2);
+    }
     public static void main(String args[]){
-        
+        Scanner s = new Scanner(System.in);
+        int n=s.nextInt();
+        System.out.print(Solve(n));
     }
 }
