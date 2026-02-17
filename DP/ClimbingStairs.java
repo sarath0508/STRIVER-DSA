1f749ae9ad4f65de287753d5cd1d1e53cc728fea
sarath0508 <itssarath05@gmail.com>
2026-02-17 16:10:30 +0530

    Using Space Optimised DP

--- DP/ClimbingStairs.java
+++ DP/ClimbingStairs.java
@@ -8,7 +8,7 @@ class ClimbingStairs {
     }
     */
 
-    /*Using Memoization */
+    /*Using Memoization
     private static int Solve(int n,int mem[]){
         if(mem[n] != 0)
             return mem[n];
@@ -16,9 +16,21 @@ class ClimbingStairs {
             return n;
         return mem[n] = Solve(n-1,mem) + Solve(n-2,mem);
     }
+     */
+
+    /*Using Space Optimised DP */
+    private static int Solve(int n){
+        int dp[] = new int[n+1];
+        dp[0] = 1; // 1-step can be reached in 1 way only
+        dp[1] = 2; // 2-step can be reached in 1+1 or 2 directly so totally 2 ways
+        for(int i = 2; i <= n; i++)
+            dp[i] = dp[i-1] + dp[i-2];
+        return dp[n-1];
+    }
+
     public static void main(String args[]){
         Scanner s = new Scanner(System.in);
         int n=s.nextInt();
-        System.out.print(Solve(n,new int[n+1]));
+        System.out.print(Solve(n));
     }
 }
